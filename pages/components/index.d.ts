8d3a75c7a3f7ebf9af2dba56c944aa2522d6da70
GuthrieW <wesleyeguthrie@gmail.com>
2021-03-30 19:30:14 -0500

    Add data to the user type

--- pages/components/index.d.ts
+++ pages/components/index.d.ts
@@ -1,5 +1,15 @@
 export type User = {
+  uid: number
   username: string
+  password: string
+  salt: string
+  loginkey: string
+  email: string
+  postnum: number
+  threadnum: number
+  avatar: string
+  avatardimensions: string
+  avatartype: string
 }
 
 export type Card = {
