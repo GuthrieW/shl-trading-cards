fa31be72850bc977dddca2ef776f81f1a7ad1012
GuthrieW <wesleyeguthrie@gmail.com>
2021-06-15 23:38:10 -0400

    Added authentication endpoint

--- utils/authentication/authenticate-user.ts
+++ utils/authentication/authenticate-user.ts
@@ -1,3 +1,17 @@
-const authenticateUser = () => {}
+import axios from 'axios'
+
+const authenticateUser = async () => {
+  const url =
+    process.env.NODE_ENV === 'production'
+      ? 'https://simulationhockey.com/userinfo.php'
+      : 'https://dev.simulationhockey.com/userinfo.php'
+
+  const result = await axios({
+    method: 'GET',
+    url: url,
+  })
+
+  return result
+}
 
 export default authenticateUser
