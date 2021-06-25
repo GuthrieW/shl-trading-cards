db4829714c54e03ce0fc5796e0b1de2cdfc4ead0
GuthrieW <wesleyeguthrie@gmail.com>
2021-06-25 00:55:59 -0400

    Added file upload preview

--- pages/dashboard/submit-cards/card-form.tsx
+++ pages/dashboard/submit-cards/card-form.tsx
@@ -16,11 +16,19 @@ const CardForm = () => {
   const [cardData, setCardData] = useState(null)
   const [canSubmit, setCanSubmit] = useState(false)
   const [isSkater, setIsSkater] = useState(true)
+  const [selectedFile, setSelectedFile] = useState(null)
 
   useEffect(() => {
     setIsSkater(cardData?.position !== 'G')
     setCanSubmit(true)
   }, [cardData])
+
+  const onFileChange = (event) => {
+    const file = URL.createObjectURL(event.target.files[0])
+    setSelectedFile(file)
+
+    console.log(event.target.files)
+  }
 
   const handleSubmit = () => {
     const formData = isSkater
@@ -55,199 +63,206 @@ const CardForm = () => {
   }
 
   return (
-    <FormGroup>
-      <TextField
-        label={'Player Name'}
-        value={cardData?.playerName || ''}
-        onChange={(event) => {
-          setCardData({
-            ...cardData,
-            ...{ playerName: event.target.value },
-          })
-        }}
-      />
-      <InputLabel id={'team-label'}>Team</InputLabel>
-      <Select
-        labelId={'team-label'}
-        value={cardData?.team || ''}
-        onChange={(event) => {
-          setCardData({
-            ...cardData,
-            ...{ team: event.target.value },
-          })
-        }}
-      >
-        {Object.entries(teams).map((team) => (
-          <MenuItem
-            key={`${team[1].City} ${team[1].Team}`}
-            value={`${team[1].City} ${team[1].Team}`}
-          >
-            {`${team[1].City} ${team[1].Team}`}
-          </MenuItem>
-        ))}
-      </Select>
-      <InputLabel id={'rarity-label'}>Rarity</InputLabel>
-      <Select
-        labelId={'rarity-label'}
-        value={cardData?.rarity || ''}
-        onChange={(event) => {
-          setCardData({
-            ...cardData,
-            ...{ rarity: event.target.value },
-          })
-        }}
-      >
-        {Object.entries(rarities).map((rarity) => (
-          <MenuItem key={rarity[1]} value={rarity[1]}>
-            {rarity[1]}
-          </MenuItem>
-        ))}
-      </Select>
-      <InputLabel id={'position-label'}>Position</InputLabel>
-      <Select
-        labelId={'position-label'}
-        value={cardData?.position || ''}
-        onChange={(event) => {
-          setCardData({
-            ...cardData,
-            ...{ position: event.target.value },
-          })
-        }}
-      >
-        {Object.entries(positions).map((position) => (
-          <MenuItem
-            key={`${position[1].label}`}
-            value={`${position[1].abbreviation}`}
-          >
-            {`${position[1].label}`}
-          </MenuItem>
-        ))}
-      </Select>
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 99 } }}
-        label={attributes.Overall}
-        value={cardData?.overall || ''}
-        onChange={(event) => {
-          setCardData({
-            ...cardData,
-            ...{ overall: event.target.value },
-          })
-        }}
-      />
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 20 } }}
-        label={
-          isSkater ? attributes.Skater.Skating : attributes.Goalie.HighShots
-        }
-        value={(isSkater ? cardData?.skating : cardData?.highShots) || ''}
-        onChange={(event) => {
-          isSkater
-            ? setCardData({
-                ...cardData,
-                ...{ skating: event.target.value },
-              })
-            : setCardData({
-                ...cardData,
-                ...{ highShots: event.target.value },
-              })
-        }}
-      />
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 20 } }}
-        label={
-          isSkater ? attributes.Skater.Shooting : attributes.Goalie.LowShots
-        }
-        value={(isSkater ? cardData?.shooting : cardData?.lowShots) || ''}
-        onChange={(event) => {
-          isSkater
-            ? setCardData({
-                ...cardData,
-                ...{ shooting: event.target.value },
-              })
-            : setCardData({
-                ...cardData,
-                ...{ lowShots: event.target.value },
-              })
-        }}
-      />
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 20 } }}
-        label={isSkater ? attributes.Skater.Hands : attributes.Goalie.Quickness}
-        value={(isSkater ? cardData?.hands : cardData?.quickness) || ''}
-        onChange={(event) => {
-          isSkater
-            ? setCardData({
-                ...cardData,
-                ...{ hands: event.target.value },
-              })
-            : setCardData({
-                ...cardData,
-                ...{ quickness: event.target.value },
-              })
-        }}
-      />
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 20 } }}
-        label={
-          isSkater ? attributes.Skater.Checking : attributes.Goalie.Control
-        }
-        value={(isSkater ? cardData?.checking : cardData?.control) || ''}
-        onChange={(event) => {
-          isSkater
-            ? setCardData({
-                ...cardData,
-                ...{ checking: event.target.value },
-              })
-            : setCardData({
-                ...cardData,
-                ...{ control: event.target.value },
-              })
-        }}
-      />
-      <TextField
-        type={'number'}
-        InputProps={{ inputProps: { min: 0, max: 20 } }}
-        label={
-          isSkater ? attributes.Skater.Defense : attributes.Goalie.Conditioning
-        }
-        value={(isSkater ? cardData?.defense : cardData?.conditioning) || ''}
-        onChange={(event) => {
-          isSkater
-            ? setCardData({
-                ...cardData,
-                ...{ defense: event.target.value },
-              })
-            : setCardData({
-                ...cardData,
-                ...{ conditioning: event.target.value },
-              })
-        }}
-      />
-      <Box m={2}>
-        <Button
-          variant="contained"
-          component="label"
-          style={{ alignSelf: 'center', marginRight: '10px' }}
+    <>
+      <FormGroup>
+        <TextField
+          label={'Player Name'}
+          value={cardData?.playerName || ''}
+          onChange={(event) => {
+            setCardData({
+              ...cardData,
+              ...{ playerName: event.target.value },
+            })
+          }}
+        />
+        <InputLabel id={'team-label'}>Team</InputLabel>
+        <Select
+          labelId={'team-label'}
+          value={cardData?.team || ''}
+          onChange={(event) => {
+            setCardData({
+              ...cardData,
+              ...{ team: event.target.value },
+            })
+          }}
         >
-          Upload Card Image
-          <input type="file" hidden />
-        </Button>
-        <Button
-          disabled={!canSubmit}
-          style={{ alignSelf: 'center' }}
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
+          {Object.entries(teams).map((team) => (
+            <MenuItem
+              key={`${team[1].City} ${team[1].Team}`}
+              value={`${team[1].City} ${team[1].Team}`}
+            >
+              {`${team[1].City} ${team[1].Team}`}
+            </MenuItem>
+          ))}
+        </Select>
+        <InputLabel id={'rarity-label'}>Rarity</InputLabel>
+        <Select
+          labelId={'rarity-label'}
+          value={cardData?.rarity || ''}
+          onChange={(event) => {
+            setCardData({
+              ...cardData,
+              ...{ rarity: event.target.value },
+            })
+          }}
         >
-          Submit Card
-        </Button>
-      </Box>
-    </FormGroup>
+          {Object.entries(rarities).map((rarity) => (
+            <MenuItem key={rarity[1]} value={rarity[1]}>
+              {rarity[1]}
+            </MenuItem>
+          ))}
+        </Select>
+        <InputLabel id={'position-label'}>Position</InputLabel>
+        <Select
+          labelId={'position-label'}
+          value={cardData?.position || ''}
+          onChange={(event) => {
+            setCardData({
+              ...cardData,
+              ...{ position: event.target.value },
+            })
+          }}
+        >
+          {Object.entries(positions).map((position) => (
+            <MenuItem
+              key={`${position[1].label}`}
+              value={`${position[1].abbreviation}`}
+            >
+              {`${position[1].label}`}
+            </MenuItem>
+          ))}
+        </Select>
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 99 } }}
+          label={attributes.Overall}
+          value={cardData?.overall || ''}
+          onChange={(event) => {
+            setCardData({
+              ...cardData,
+              ...{ overall: event.target.value },
+            })
+          }}
+        />
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 20 } }}
+          label={
+            isSkater ? attributes.Skater.Skating : attributes.Goalie.HighShots
+          }
+          value={(isSkater ? cardData?.skating : cardData?.highShots) || ''}
+          onChange={(event) => {
+            isSkater
+              ? setCardData({
+                  ...cardData,
+                  ...{ skating: event.target.value },
+                })
+              : setCardData({
+                  ...cardData,
+                  ...{ highShots: event.target.value },
+                })
+          }}
+        />
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 20 } }}
+          label={
+            isSkater ? attributes.Skater.Shooting : attributes.Goalie.LowShots
+          }
+          value={(isSkater ? cardData?.shooting : cardData?.lowShots) || ''}
+          onChange={(event) => {
+            isSkater
+              ? setCardData({
+                  ...cardData,
+                  ...{ shooting: event.target.value },
+                })
+              : setCardData({
+                  ...cardData,
+                  ...{ lowShots: event.target.value },
+                })
+          }}
+        />
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 20 } }}
+          label={
+            isSkater ? attributes.Skater.Hands : attributes.Goalie.Quickness
+          }
+          value={(isSkater ? cardData?.hands : cardData?.quickness) || ''}
+          onChange={(event) => {
+            isSkater
+              ? setCardData({
+                  ...cardData,
+                  ...{ hands: event.target.value },
+                })
+              : setCardData({
+                  ...cardData,
+                  ...{ quickness: event.target.value },
+                })
+          }}
+        />
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 20 } }}
+          label={
+            isSkater ? attributes.Skater.Checking : attributes.Goalie.Control
+          }
+          value={(isSkater ? cardData?.checking : cardData?.control) || ''}
+          onChange={(event) => {
+            isSkater
+              ? setCardData({
+                  ...cardData,
+                  ...{ checking: event.target.value },
+                })
+              : setCardData({
+                  ...cardData,
+                  ...{ control: event.target.value },
+                })
+          }}
+        />
+        <TextField
+          type={'number'}
+          InputProps={{ inputProps: { min: 0, max: 20 } }}
+          label={
+            isSkater
+              ? attributes.Skater.Defense
+              : attributes.Goalie.Conditioning
+          }
+          value={(isSkater ? cardData?.defense : cardData?.conditioning) || ''}
+          onChange={(event) => {
+            isSkater
+              ? setCardData({
+                  ...cardData,
+                  ...{ defense: event.target.value },
+                })
+              : setCardData({
+                  ...cardData,
+                  ...{ conditioning: event.target.value },
+                })
+          }}
+        />
+        <Box m={2}>
+          <Button
+            variant="contained"
+            component="label"
+            style={{ alignSelf: 'center', marginRight: '10px' }}
+          >
+            Upload Card Image
+            <input type="file" onChange={onFileChange} hidden />
+          </Button>
+          <Button
+            disabled={!canSubmit}
+            style={{ alignSelf: 'center' }}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+          >
+            Submit Card
+          </Button>
+        </Box>
+      </FormGroup>
+      {selectedFile && <img className={null} src={selectedFile} />}
+    </>
   )
 }
 
