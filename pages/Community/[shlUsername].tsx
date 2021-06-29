4217f8b6785ca330b2fc630d37aba8e99a4cdc7e
GuthrieW <wesleyeguthrie@gmail.com>
2021-06-29 14:21:16 -0400

    Changed import to use absolute path

--- pages/Community/[shlUsername].tsx
+++ pages/Community/[shlUsername].tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import Collection from '../collection'
+import Collection from '@pages/collection'
 
 const CommunityMemberPage = () => {
   const router = useRouter()
