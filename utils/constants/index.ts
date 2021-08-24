e28031cede2e2ee628e5f47cbf5c2f43db269cdf
GuthrieW <wesleyeguthrie@gmail.com>
2021-08-24 19:24:56 -0400

    Different impot

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,5 +1,5 @@
 export { default as storageKeys } from './storage-keys'
-export { default as rarities } from './rarities'
+// export { default as rarities } from './rarities'
 export { default as teams } from './teams'
 export { default as attributes } from './attributes'
 export { default as cardData } from './card-data'
