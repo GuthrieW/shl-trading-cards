a8db14418fb22b1867f7f2fe280849dbb3bfae61
GuthrieW <wesleyeguthrie@gmail.com>
2021-09-02 01:48:47 -0400

    Added basic pack purchasing page

---
+++ pages/collection/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Collection = () => {
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>placeholder</h1>
+    </>
+  )
+}
+
+export default Collection
---
+++ pages/trade-hub/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TradeHub = () => {
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>placeholder</h1>
+    </>
+  )
+}
+
+export default TradeHub
