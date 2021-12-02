8f7bf638d889f800cd2caadc3df65482e1a1ee8d
GuthrieW <wesleyeguthrie@gmail.com>
2021-12-01 22:20:35 -0500

    MuiDataTable fix

--- components/data-table/index.tsx
+++ components/data-table/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import MUIDataTable from 'mui-datatables'
 
 const DataTable = ({ title, data, columns, options }) => {
-  return (
+  return typeof window !== 'undefined' ? (
     <MUIDataTable
       title={title}
       data={data}
       columns={columns}
       options={options}
     />
-  )
+  ) : null
 }
 
 export default DataTable
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export { default as InfoCard } from './info-card'
 export { default as Loading } from './loading'
 export { default as CardTable } from './card-table'
-export { default as UserTable } from './user-table'
 export { default as DataTable } from './data-table'
