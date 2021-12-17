4639216145b53603e07b9b3c9cc372f84eafa980
GuthrieW <wesleyeguthrie@gmail.com>
2021-12-17 12:28:36 -0500

    Changed path to use https

--- constants/path-to-cards/index.ts
+++ constants/path-to-cards/index.ts
@@ -1,5 +1,5 @@
 export const pathToCardsForUpload =
   '../../simulationhockey.com/public_html/tradingcards/'
-export const pathToCards = 'http://simulationhockey.com/tradingcards/'
+export const pathToCards = 'https://simulationhockey.com/tradingcards/'
 
 export default pathToCards
