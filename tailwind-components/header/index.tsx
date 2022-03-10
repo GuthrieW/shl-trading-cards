572297f3b98d3d8c4ed966c6fd552eac9cff1a91
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-10 11:41:25 -0500

    added nvmrc

--- tailwind-components/header/index.tsx
+++ tailwind-components/header/index.tsx
@@ -4,11 +4,11 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 const Header = () => (
   <div className="relative top-0 w-full h-16 bg-gray-600 flex flex-row items-center">
     <div className="flex flex-row mx-2 cursor-pointer">
-      <span>Home</span>
+      <span className="text-red-800">Home</span>
       <ChevronDownIcon className="ml-2 h-5 w-5" />
     </div>
     <div className="flex flex-row mx-2 cursor-pointer">
-      <span>Collections</span>
+      <span className=" text-orange-800">Collections</span>
       <ChevronDownIcon className="ml-2 h-5 w-5" />
     </div>
     <div className="flex flex-row mx-2 cursor-pointer">
