ae08f849adde66a3bc488fab759bb1512d0b0525
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-10 11:53:10 -0500

    upgrade to next v12

--- tailwind-components/header/index.tsx
+++ tailwind-components/header/index.tsx
@@ -4,11 +4,11 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 const Header = () => (
   <div className="relative top-0 w-full h-16 bg-gray-600 flex flex-row items-center">
     <div className="flex flex-row mx-2 cursor-pointer">
-      <span className="text-red-800">Home</span>
+      <span className="text-green-200">Home</span>
       <ChevronDownIcon className="ml-2 h-5 w-5" />
     </div>
     <div className="flex flex-row mx-2 cursor-pointer">
-      <span className=" text-orange-800">Collections</span>
+      <span>Collections</span>
       <ChevronDownIcon className="ml-2 h-5 w-5" />
     </div>
     <div className="flex flex-row mx-2 cursor-pointer">
