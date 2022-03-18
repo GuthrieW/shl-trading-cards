311911a352281058bd5ca12f476f1c29fc66069c
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-18 01:08:38 -0400

    Added some basic pack viewing animations and css

--- pages/open-packs/last-pack/index.tsx
+++ pages/open-packs/last-pack/index.tsx
@@ -1,12 +1,26 @@
 import { useGetLatestPackCards } from '@pages/api/queries'
 import getUidFromSession from '@utils/get-uid-from-session'
-import React from 'react'
+import React, { useState } from 'react'
 import { pathToCards } from '@constants/index'
 
 const LastOpenedPack = () => {
   const { latestPackCards, isLoading, isError } = useGetLatestPackCards({
     uid: getUidFromSession(),
   })
+
+  const [cards, setCards] = useState<any[]>(latestPackCards)
+
+  const handleUpdateCard = (index) => {
+    const updatedArray = cards.map((card, i) => {
+      if (i === index) {
+        return { ...card, flipped: true }
+      } else {
+        return card
+      }
+    })
+
+    setCards(updatedArray)
+  }
 
   if (isLoading || isError) return null
 
@@ -16,10 +30,28 @@ const LastOpenedPack = () => {
     <div className="m-2">
       <h1>Last Opened Pack</h1>
       <div className="flex justify-center items-center">
-        <div className="grid grid-cols-3 gap-5">
-          {latestPackCards.map((card, index) => (
-            <img key={index} src={`${pathToCards}${card.image_url}`} />
-          ))}
+        <div className="grid grid-cols-6 gap-2 overflow-x-auto">
+          {cards.map((card, index) => {
+            return card.flipped ? (
+              <img
+                width="320"
+                height="440"
+                key={index}
+                className="animate-slide-in-left"
+                onClick={() => handleUpdateCard(index)}
+                src={`${pathToCards}${card.image_url}`}
+              />
+            ) : (
+              <img
+                width="320"
+                height="440"
+                key={index}
+                className="animate-slide-in-left"
+                onClick={() => handleUpdateCard(index)}
+                src={`https://m.media-amazon.com/images/I/71pl1bc87ZL._AC_SY445_.jpg`}
+              />
+            )
+          })}
         </div>
       </div>
     </div>
