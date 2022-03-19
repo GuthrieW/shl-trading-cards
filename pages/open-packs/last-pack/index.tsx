c70cdd64ef160311b1d13b5c1bdae9286cb8fc47
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-19 14:30:58 -0400

    Basic last pack page

--- pages/open-packs/last-pack/index.tsx
+++ pages/open-packs/last-pack/index.tsx
@@ -8,50 +8,22 @@ const LastOpenedPack = () => {
     uid: getUidFromSession(),
   })
 
-  const [cards, setCards] = useState<any[]>(latestPackCards)
-
-  const handleUpdateCard = (index) => {
-    const updatedArray = cards.map((card, i) => {
-      if (i === index) {
-        return { ...card, flipped: true }
-      } else {
-        return card
-      }
-    })
-
-    setCards(updatedArray)
-  }
-
-  if (isLoading || isError) return null
-
-  if (latestPackCards === []) return null
+  if (isLoading || isError || latestPackCards === []) return null
 
   return (
     <div className="m-2">
       <h1>Last Opened Pack</h1>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-6 gap-2 overflow-x-auto">
-          {cards.map((card, index) => {
-            return card.flipped ? (
-              <img
-                width="320"
-                height="440"
-                key={index}
-                className="animate-slide-in-left"
-                onClick={() => handleUpdateCard(index)}
-                src={`${pathToCards}${card.image_url}`}
-              />
-            ) : (
-              <img
-                width="320"
-                height="440"
-                key={index}
-                className="animate-slide-in-left"
-                onClick={() => handleUpdateCard(index)}
-                src={`https://m.media-amazon.com/images/I/71pl1bc87ZL._AC_SY445_.jpg`}
-              />
-            )
-          })}
+          {latestPackCards.map((card, index) => (
+            <img
+              width="320"
+              height="440"
+              key={index}
+              className="animate-slide-in-left"
+              src={`${pathToCards}${card.image_url}`}
+            />
+          ))}
         </div>
       </div>
     </div>
