5ca96b86a4dc8748f6e7f31f8661133fbfe6e38a
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-23 23:50:43 -0400

    Updated pack pull percentages

--- pages/api/v2/packs/open/[packID].ts
+++ pages/api/v2/packs/open/[packID].ts
@@ -22,21 +22,89 @@ const randomIntFromInterval = (maximum: number): number => {
  * Hall of Fame 0.15%
  * Diamond - 1.35%
  * Ruby - 3%
+ * Logo - 3%
  * Gold - 25.5%
- * Silver - 30%
- * Bronze - 39.5%
- * Logo - 0.5%
+ * Silver - 28.5%
+ * Bronze - 38.5%
  */
 
 const getBasePackRarity = (): string => {
   const num = randomIntFromInterval(10000)
-  if (num > 0 && num <= 15) return rarityMap.hallOfFame.label
-  if (num > 15 && num <= 150) return rarityMap.diamond.label
-  if (num > 150 && num <= 450) return rarityMap.ruby.label
-  if (num > 450 && num <= 3000) return rarityMap.gold.label
-  if (num > 3000 && num <= 6000) return rarityMap.silver.label
-  if (num > 6000 && num <= 9950) return rarityMap.bronze.label
-  if (num > 9950 && num <= 10000) return rarityMap.logo.label
+
+  if (num > 0 && num <= rarityMap.hallOfFame.rarity)
+    return rarityMap.hallOfFame.label
+  if (
+    num > rarityMap.hallOfFame.rarity &&
+    num <= rarityMap.hallOfFame.rarity + rarityMap.diamond.rarity
+  )
+    return rarityMap.diamond.label
+  if (
+    num > rarityMap.hallOfFame.rarity + rarityMap.diamond.rarity &&
+    num <=
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity
+  )
+    return rarityMap.ruby.label
+  if (
+    num >
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity &&
+    num <=
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity
+  )
+    return rarityMap.gold.label
+  if (
+    num >
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity &&
+    num <=
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity +
+        rarityMap.silver.rarity
+  )
+    return rarityMap.silver.label
+  if (
+    num >
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity +
+        rarityMap.silver.rarity &&
+    num <=
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity +
+        rarityMap.silver.rarity +
+        rarityMap.bronze.rarity
+  )
+    return rarityMap.bronze.label
+  if (
+    num >
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity +
+        rarityMap.silver.rarity +
+        rarityMap.bronze.rarity &&
+    num <=
+      rarityMap.hallOfFame.rarity +
+        rarityMap.diamond.rarity +
+        rarityMap.ruby.rarity +
+        rarityMap.gold.rarity +
+        rarityMap.silver.rarity +
+        rarityMap.logo.rarity
+  )
+    return rarityMap.logo.label
   return rarityMap.bronze.label
 }
 
