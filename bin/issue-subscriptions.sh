8872a4f9ab57b0a710bb6eee1777af34f93b9289
Wesley Guthrie <wesleyeguthrie@gmail.com>
2022-03-26 19:18:09 -0400

    more script testing

--- bin/issue-subscriptions.sh
+++ bin/issue-subscriptions.sh
@@ -3,6 +3,11 @@
 subscribed_users=$(mysql admin_cards -u admin_cards csv_import -CTRFcardMaster99! -e "SELECT userID, subscription FROM admin_cards.settings WHERE subscription > 0;")
 echo subscribed_users
 
+for user in $subscribed_users
+do  
+   echo $user
+done
+
 # Cancel subscriptions for users who don't have enough money
 
 # Notfiy users of their cancelled subscriptions
