33d8498c069217ef1f72483eb0ed0fbf37f742c1
Evan Silverman <es4753@nyu.edu>
2022-03-28 19:18:21 -0400

    add encrypted tokens to site so users have a harder time doing bad stuff

--- utils/get-uid-from-session/index.ts
+++ utils/get-uid-from-session/index.ts
@@ -1,6 +1,18 @@
+import CryptoJS from 'crypto-js'
+
 const getUidFromSession = () => {
   if (typeof window !== 'undefined') {
-    return parseInt(sessionStorage.getItem('uid'))
+    const token = window.sessionStorage.getItem('token')
+
+    console.log('token', process.env.NEXT_PUBLIC_TOKEN_KEY)
+    return token
+      ? parseInt(
+          CryptoJS.AES.decrypt(
+            token,
+            process.env.NEXT_PUBLIC_TOKEN_KEY
+          ).toString(CryptoJS.enc.Utf8)
+        )
+      : parseInt(window.sessionStorage.getItem('uid') || '0')
   } else {
     return null
   }
