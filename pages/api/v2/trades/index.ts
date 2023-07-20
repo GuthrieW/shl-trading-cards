7d7e690b91e0754138ebca79309e22422f4a1778
Wesley Guthrie <wesleyeguthrie@gmail.com>
2023-07-20 12:56:27 -0500

    add double check for if user owns card

--- pages/api/v2/trades/index.ts
+++ pages/api/v2/trades/index.ts
@@ -4,7 +4,10 @@ import Cors from 'cors'
 import { POST } from '@constants/http-methods'
 import SQL from 'sql-template-strings'
 import { StatusCodes } from 'http-status-codes'
-import { queryDatabase } from '@pages/api/database/database'
+import {
+  getCardsDatabaseName,
+  queryDatabase,
+} from '@pages/api/database/database'
 import { TradeAsset } from '@pages/api/mutations/use-create-trade'
 
 const allowedMethods = [POST]
@@ -44,6 +47,27 @@ const index = async (
         .send('You cannot trade with yourself')
       return
     }
+
+    const cardOwners = await Promise.all(
+      tradeAssets.map(async (asset: TradeAsset) => {
+        const cardOwner = await queryDatabase(
+          SQL`SELECT userID FROM `
+            .append(getCardsDatabaseName())
+            .append(`.collection WHERE ownedCardId=`)
+            .append(asset.ownedCardId)
+            .append(` LIMIT 1;`)
+        )
+
+        if (cardOwner[0].userID != asset.fromId) {
+          response
+            .status(StatusCodes.BAD_REQUEST)
+            .send(`User ${asset.fromId} does not own card ${asset.ownedCardId}`)
+          return
+        }
+      })
+    )
+
+    console.log('cardOwners', cardOwners)
 
     const createTradeResult = await queryDatabase(
       SQL`call create_trade(${initiatorId},${recipientId})`
