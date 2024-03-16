fda58eb16d985c5861eef507e59f282817f0f150
Wesley Guthrie <wesleyeguthrie@gmail.com>
2024-03-16 18:52:26 -0500

    comment

--- lib/imageService.ts
+++ lib/imageService.ts
@@ -3,15 +3,30 @@ import { pathToCardsForUpload } from '@constants/path-to-cards'
 import { v4 as uuid } from 'uuid'
 
 class ImageService {
-  async convertToWebp(image: string): Promise<Buffer> {
+  /**
+   *
+   * @param {string} image
+   * @returns {Buffer}
+   */
+  async convertToWebp(image: ArrayBuffer | Buffer | string): Promise<Buffer> {
     return await sharp(image).webp().toBuffer()
   }
 
+  /**
+   *
+   * @param {Buffer} imageBuffer
+   * @param {string} filename
+   * @returns {OutputInfo}
+   */
   async saveImage(imageBuffer: Buffer, filename: string): Promise<OutputInfo> {
     return await sharp(imageBuffer).toFile(`${pathToCardsForUpload}${filename}`)
   }
 
-  async generateFilename() {
+  /**
+   *
+   * @returns {string}
+   */
+  generateFilename(): string {
     return `${uuid()}.webp`
   }
 }
