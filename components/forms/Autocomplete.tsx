988078b34915939f13b3ce843b781587b2f9fbce
luketd <lukedamato99@gmail.com>
2024-10-19 22:39:02 -0400

    cleaned up Autocomplete, as well as #176

--- components/forms/Autocomplete.tsx
+++ components/forms/Autocomplete.tsx
@@ -1,3 +1,11 @@
+import React, { useMemo, useRef, useState, useEffect } from 'react'
+import { useDebounce } from 'use-debounce'
+import axios from 'axios'
+import { useSession } from 'contexts/AuthContext'
+import { GET } from '@constants/http-methods'
+import { query } from '@pages/api/database/query'
+import { ListResponse } from '@pages/api/v3'
+import { UserData } from '@pages/api/v3/user'
 import {
   Alert,
   AlertIcon,
@@ -5,18 +13,9 @@ import {
   FormControl,
   FormLabel,
   Input,
-  useDisclosure,
 } from '@chakra-ui/react'
-import { GET } from '@constants/http-methods'
-import { query } from '@pages/api/database/query'
-import { ListResponse } from '@pages/api/v3'
-import { UserData } from '@pages/api/v3/user'
-import axios from 'axios'
-import { useSession } from 'contexts/AuthContext'
-import { useMemo, useRef, useState } from 'react'
-import { useDebounce } from 'use-debounce'
 
-export type AutocomleteOption = { label: string; value: string }
+export type AutocompleteOption = { label: string; value: string }
 
 export default function Autocomplete({
   label,
@@ -26,10 +25,11 @@ export default function Autocomplete({
   onSelect: (s: string) => void
 }) {
   const { session } = useSession()
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [isOpen, setIsOpen] = useState(false)
   const [inputText, setInputText] = useState<string>('')
   const [debouncedUsername] = useDebounce(inputText, 500)
-  const inputRef = useRef()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const dropdownRef = useRef<HTMLDivElement>(null)
 
   const { payload: usersWithCards } = query<ListResponse<UserData>>({
     queryKey: ['with-cards', debouncedUsername],
@@ -44,34 +44,50 @@ export default function Autocomplete({
       }),
   })
 
-  const usersWithCardsOptions: AutocomleteOption[] = useMemo(
+  const usersWithCardsOptions: AutocompleteOption[] = useMemo(
     () =>
       usersWithCards?.rows.map((user) => ({
         label: user.username,
         value: String(user.uid),
-      })),
+      })) || [],
     [usersWithCards]
   )
 
   const handleFocusInput = () => {
-    // @ts-ignore
-    inputRef.current.value = ''
-    setInputText('')
-    onSelect(null)
-    onOpen()
+    setIsOpen(true)
   }
 
-  const handleChangeInput = (event) => {
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value)
+    setIsOpen(true)
   }
 
-  const handleClickButton = (newOption: AutocomleteOption) => {
-    // @ts-ignore
-    inputRef.current.value = newOption.label
+  const handleClickButton = (newOption: AutocompleteOption) => {
+    if (inputRef.current) {
+      inputRef.current.value = newOption.label
+    }
     setInputText(newOption.label)
     onSelect(newOption.value)
-    onClose()
+    setIsOpen(false)
   }
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) &&
+        inputRef.current &&
+        !inputRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
   return (
     <div className="relative">
@@ -81,24 +97,33 @@ export default function Autocomplete({
           ref={inputRef}
           onFocus={handleFocusInput}
           onChange={handleChangeInput}
+          value={inputText}
         />
-        <div className="absolute flex flex-col justify-start items-start w-full bg-primaryDark z-20">
-          {isOpen &&
-            usersWithCardsOptions?.map((option) => (
+        {isOpen && (
+          <div
+            ref={dropdownRef}
+            className="absolute flex flex-col justify-start items-start w-full bg-primary z-20 max-h-60 overflow-y-auto"
+          >
+            {usersWithCardsOptions.map((option) => (
               <Button
                 key={option.value}
-                className={`w-full rounded-none flex justify-start items-center`}
+                _focus={{
+                  bg: 'blue.100',
+                  boxShadow: 'outline',
+                }}
+                className="w-full rounded-none flex justify-start items-center focus:outline-none focus:ring-2 focus:ring-blue-500"
                 onClick={() => handleClickButton(option)}
               >
                 {option.label}
               </Button>
             ))}
-        </div>
+          </div>
+        )}
       </FormControl>
       {debouncedUsername?.length > 0 && debouncedUsername?.length < 3 && (
         <Alert status="info">
           <AlertIcon />
-          At least three charaters required to search for a username
+          At least three characters are required to search for a username
         </Alert>
       )}
     </div>
