19d9272ab903b980a39311adb2dd10a1bdc52523
luketd <lukedamato99@gmail.com>
2024-10-22 23:06:34 -0400

    switched to react icons
    hero icons didnt work on mobile + fully switching to react icons instead

--- components/table/TablePagination.tsx
+++ components/table/TablePagination.tsx
@@ -1,10 +1,10 @@
 import { Box, IconButton } from '@chakra-ui/react'
 import {
-  ChevronDoubleLeftIcon,
-  ChevronDoubleRightIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-} from '@heroicons/react/20/solid'
+ArrowLeftIcon,
+ArrowRightIcon,
+ChevronLeftIcon,
+ChevronRightIcon
+} from '@chakra-ui/icons'
 import { useEffect, useState } from 'react'
 
 export default ({
@@ -56,13 +56,13 @@ export default ({
         onClick={goToFirstPage}
         className="mx-2 cursor-pointer"
         aria-label="go-to-first-page"
-        icon={<ChevronDoubleLeftIcon />}
+        icon={<ArrowLeftIcon />}
       />
       <IconButton
         onClick={goToPreviousPage}
         className="mx-2 cursor-pointer"
         aria-label="go-to-previous-page"
-        icon={<ChevronLeftIcon />}
+        icon={<ChevronLeftIcon fontSize="2xl" />}
       />
       <span className="flex justify-center items-center mx-2">
         {currentPage ?? 0} of {totalPages ?? 0}
@@ -71,13 +71,13 @@ export default ({
         onClick={goToNextPage}
         className="mx-2 cursor-pointer"
         aria-label="go-to-next-page"
-        icon={<ChevronRightIcon />}
+        icon={<ChevronRightIcon fontSize="2xl" />}
       />
       <IconButton
         onClick={goToLastPage}
         className="mx-2 cursor-pointer"
         aria-label="go-to-final-page"
-        icon={<ChevronDoubleRightIcon />}
+        icon={<ArrowRightIcon />}
       />
     </Box>
   )
