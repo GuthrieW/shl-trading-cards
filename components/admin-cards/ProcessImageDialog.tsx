99f6e9776c9b47417e3e90e438770661ecd409c8
luketd <lukedamato99@gmail.com>
2024-10-25 14:07:23 -0400

    Updated process image to scale on mobile and added more info for the approvers to process

--- components/admin-cards/ProcessImageDialog.tsx
+++ components/admin-cards/ProcessImageDialog.tsx
@@ -13,7 +13,9 @@ import {
   AlertDialogHeader,
   AlertDialogOverlay,
   AlertIcon,
+  Box,
   Button,
+  Flex,
   Image,
   useToast,
 } from '@chakra-ui/react'
@@ -32,7 +34,6 @@ export default function ProcessImageDialog({
   const [formError, setFormError] = useState<string>(null)
   const cancelRef = useRef(null)
   const queryClient = useQueryClient()
-
   const { mutateAsync: approveImage, isLoading: approveIsLoading } = mutation<
     void,
     { cardID: number }
@@ -104,26 +105,104 @@ export default function ProcessImageDialog({
       isOpen={isOpen}
       leastDestructiveRef={cancelRef}
       onClose={onClose}
+      size={{ base: 'xs', sm: 'md', md: 'lg', lg: 'lg' }}
     >
       <AlertDialogOverlay>
         <AlertDialogContent>
-          <AlertDialogHeader fontSize="lg" fontWeight="bold" className="!bg-primary !text-secondary">
+          <AlertDialogHeader
+            fontSize="lg"
+            fontWeight="bold"
+            className="!bg-primary !div-secondary"
+          >
             Process Image #{card.cardID}
           </AlertDialogHeader>
           <AlertDialogCloseButton />
-          <AlertDialogBody className="!bg-primary !text-secondary">
+          <AlertDialogBody className="!bg-primary !div-secondary">
             <Image
-              className="cursor-pointer"
+              className="cursor-pointer select-none w-full max-w-xs sm:max-w-sm"
               src={`https://simulationhockey.com/tradingcards/${card.image_url}`}
               fallback={
-                <div className="relative z-10">
+                <Box className="relative z-10">
                   <Image src="/cardback.png" />
-                  <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-20"></div>
-                </div>
+                  <Box className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-20"></Box>
+                </Box>
               }
             />
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex space-x-2">
+                <span className="font-medium">Player:</span>
+                <span>{card.player_name}</span>
+              </div>
+              <div className="flex space-x-2">
+                <span className="font-medium">Rarity:</span>
+                <span>{card.card_rarity}</span>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-3 gap-4">
+              <div className="flex space-x-2">
+                <span className="font-medium">Position:</span>
+                <span>{card.position}</span>
+              </div>
+              <div className="flex space-x-2">
+                <span className="font-medium">Overall:</span>
+                <span>{card.overall}</span>
+              </div>
+              <div className="flex space-x-2">
+                <span className="font-medium">Season:</span>
+                <span>{card.season}</span>
+              </div>
+            </div>
+            {card.position === 'Goalie' ? (
+              <div className="grid grid-cols-3 gap-5">
+                <div className="flex space-x-2">
+                  <span className="font-medium">High:</span>
+                  <span>{card.high_shots}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Low:</span>
+                  <span>{card.low_shots}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Quick:</span>
+                  <span>{card.quickness}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Control:</span>
+                  <span>{card.control}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Cond:</span>
+                  <span>{card.conditioning}</span>
+                </div>
+              </div>
+            ) : (
+              <div className="grid grid-cols-3 gap-5">
+                <div className="flex space-x-2">
+                  <span className="font-medium">Skating:</span>
+                  <span>{card.skating}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Shooting:</span>
+                  <span>{card.shooting}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Hands:</span>
+                  <span>{card.hands}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Check:</span>
+                  <span>{card.checking}</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span className="font-medium">Defense:</span>
+                  <span>{card.defense}</span>
+                </div>
+              </div>
+            )}
           </AlertDialogBody>
-          <AlertDialogFooter className="!bg-primary !text-secondary">
+          <AlertDialogFooter className="!bg-primary !div-secondary">
             {formError && (
               <Alert status="error">
                 <AlertIcon /> {formError}
@@ -132,7 +211,7 @@ export default function ProcessImageDialog({
             <Button
               disabled={false}
               colorScheme="red"
-              className=" background-transparent font-bold uppercase px-6 py-3 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-red-100 rounded hover:shadow-lg"
+              className=" background-transparent font-bold uppercase px-6 py-3 div-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-red-100 rounded hover:shadow-lg"
               onClick={handleDeny}
             >
               Deny Card
@@ -140,7 +219,7 @@ export default function ProcessImageDialog({
             <Button
               disabled={false}
               colorScheme="green"
-              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              className="bg-emerald-500 div-white active:bg-emerald-600 font-bold uppercase div-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               onClick={handleApprove}
             >
               Accept Card
