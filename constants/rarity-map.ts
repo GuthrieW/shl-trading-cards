76bdf793e3ffae55867dbe8e077b167f866861e2
luketd <lukedamato99@gmail.com>
2024-10-25 14:10:17 -0400

    #196

--- constants/rarity-map.ts
+++ constants/rarity-map.ts
@@ -9,19 +9,19 @@ const rarityMap = {
   silver: { label: 'Silver', value: 'Silver', rarity: 2955 },
   gold: { label: 'Gold', value: 'Gold', rarity: 1500 },
   ruby: { label: 'Ruby', value: 'Ruby', rarity: 400 },
-  diamond: { label: 'Diamond', value: 'Diamond', rarity: 200 },
   logo: { label: 'Logo', value: 'Logo', rarity: 400 },
-  hallOfFame: { label: 'Hall of Fame', value: 'Hall of Fame', rarity: 15 },
-  misprint: { label: 'Misprint', value: 'Misprint', rarity: 0 },
-  charity: { label: 'Charity', value: 'Charity', rarity: 35 },
+  diamond: { label: 'Diamond', value: 'Diamond', rarity: 200 },
   award: { label: 'Awards', value: 'Awards', rarity: 100 },
   twoThousandClub: {
     label: '2000 TPE Club',
     value: '2000 TPE Club',
     rarity: 50,
   },
-  firstOverall: { label: '1st Overall', value: '1st Overall', rarity: 35 },
   iihfAwards: { label: 'IIHF Awards', value: 'IIHF Awards', rarity: 65 },
+  charity: { label: 'Charity', value: 'Charity', rarity: 35 },
+  firstOverall: { label: '1st Overall', value: '1st Overall', rarity: 35 },
+  hallOfFame: { label: 'Hall of Fame', value: 'Hall of Fame', rarity: 15 },
+  misprint: { label: 'Misprint', value: 'Misprint', rarity: 0 },
 } as const satisfies Record<string, Rarity>
 
 export default rarityMap
