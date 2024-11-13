527521a3eb7c109fdb7020cde0f4dd8ae278e672
luketd <lukedamato99@gmail.com>
2024-11-13 16:39:29 -0500

    Update BackOfCard.tsx

--- components/collection/BackOfCard.tsx
+++ components/collection/BackOfCard.tsx
@@ -62,7 +62,7 @@ export const BackOfCard: React.FC<BackOfCardProps> = ({
             <div className="text-lg">Pack #{pack.packID}</div>
             <Link
               href={`/packs/${pack.packID}`}
-              className="text-linkd"
+              className="text-link"
               as="a"
               target="_blank"
             >
