3795dbf8e350f5324b5d06e9d7e7b02ffd7dbd27
luketd <lukedamato99@gmail.com>
2024-12-08 11:18:45 -0500

    added totalCardQuantity for first pull + new badge and colors

--- pages/packs/last-pack/index.tsx
+++ pages/packs/last-pack/index.tsx
@@ -5,7 +5,7 @@ import Router, { useRouter } from 'next/router'
 import ReactCardFlip from 'react-card-flip'
 import { rarityMap } from '@constants/rarity-map'
 import pathToCards from '@constants/path-to-cards'
-import { Button, Badge, useToast } from '@chakra-ui/react' // Import Badge from Chakra UI
+import { Button, Badge, useToast, Tooltip } from '@chakra-ui/react' // Import Badge from Chakra UI
 import { PageWrapper } from '@components/common/PageWrapper'
 import { GET } from '@constants/http-methods'
 import { query } from '@pages/api/database/query'
@@ -52,7 +52,6 @@ const LastOpenedPack = () => {
     useGetLatestPackCards({
       uid: user?.uid,
     })
-
   const handleCardClick = (index: number, card: Card) => {
     if (revealedCards.includes(index)) {
       setSelectedCard(card)
@@ -99,7 +98,26 @@ const LastOpenedPack = () => {
       })
       .join(' ')
 
-    const message = `I opened pack #${packID} \n ${emojis}\nCheck out my cards: https://cards.simulationhockey.com/packs/${packID}`
+    const specialCardMessages = latestPackCards
+      .filter((card) => card.totalCardQuantity === 1 || card.quantity === 1)
+      .map((card) => {
+        if (card.totalCardQuantity === 1) {
+          return `First pull of ${card.card_rarity} ${card.player_name}! 🌟`
+        }
+        if (card.quantity === 1 && card.totalCardQuantity > 1) {
+          return `My first ${card.card_rarity} ${card.player_name}! 🆕`
+        }
+        return null
+      })
+      .filter(Boolean)
+
+    const specialCardText =
+      specialCardMessages.length > 0
+        ? '\n' + specialCardMessages.join('\n')
+        : ''
+
+    const message = `I opened pack #${packID} \n ${emojis}${specialCardText}\nCheck out my cards: https://cards.simulationhockey.com/packs/${packID}`
+
     navigator.clipboard.writeText(message).then(() => {
       toast({
         title: 'Copied to clipboard',
@@ -108,7 +126,6 @@ const LastOpenedPack = () => {
       })
     })
   }
-
   if (isLoading || isError || latestPackCards.length === 0) return null
 
   return (
@@ -186,6 +203,25 @@ const LastOpenedPack = () => {
                       >
                         New Card
                       </Badge>
+                    )}
+                  {revealedCards.includes(index) &&
+                    card.totalCardQuantity === 1 && ( // If first ever card thats been opened
+                      <Tooltip
+                        label="First pull of this card"
+                        aria-label="First pull explanation"
+                        hasArrow
+                        placement="top"
+                      >
+                        <Badge
+                          colorScheme="yellow"
+                          variant="outline"
+                          border="2px solid"
+                          borderColor="yellow.500"
+                          className="shine-effect-gold mt-2"
+                        >
+                          First Pull
+                        </Badge>
+                      </Tooltip>
                     )}
                 </div>
               ))}
