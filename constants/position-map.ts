c324379fbf18520f674d224d966ae06d9bbbb538
luketd <lukedamato99@gmail.com>
2025-06-02 16:44:44 -0400

    Update

--- constants/position-map.ts
+++ constants/position-map.ts
@@ -7,6 +7,7 @@ const positionMap = {
   forward: { label: 'Forward', value: 'F' },
   defense: { label: 'Defense', value: 'D' },
   goalie: { label: 'Goalie', value: 'G' },
+  logo: { label: 'Logo', value: 'X' },
 }
 
 export default positionMap
