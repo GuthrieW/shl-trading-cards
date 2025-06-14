7d458e409d280a850f0507bb04d7cb119801db6c
luketd <lukedamato99@gmail.com>
2025-06-14 19:55:53 -0400

    Init

---
+++ components/trades/ViewTrades.tsx
@@ -0,0 +1,129 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Stack,
+  Heading,
+} from '@chakra-ui/react'
+import ViewTradeTable from '@components/tables/ViewTradeTable'
+import { GET } from '@constants/http-methods'
+import { query } from '@pages/api/database/query'
+import { ListResponse, SortDirection } from '@pages/api/v3'
+import axios from 'axios'
+import { useSession } from 'contexts/AuthContext'
+import { useState } from 'react'
+import { useDebounce } from 'use-debounce'
+
+const TRADE_STATUS_OPTIONS: {
+  value: TradeStatus
+  label: string
+  sortLabel: (direciton: SortDirection) => string
+}[] = [
+  {
+    value: 'COMPLETE',
+    label: 'Complete',
+    sortLabel: (direction: SortDirection) =>
+      direction === 'DESC' ? '(Descending)' : '(Ascending)',
+  },
+  {
+    value: 'PENDING',
+    label: 'Pending',
+    sortLabel: (direction: SortDirection) =>
+      direction === 'DESC' ? '(Descending)' : '(Ascending)',
+  },
+  {
+    value: 'DECLINED',
+    label: 'Declined',
+    sortLabel: (direction: SortDirection) =>
+      direction === 'DESC' ? '(Descending)' : '(Ascending)',
+  },
+  {
+    value: 'AUTO_DECLINED',
+    label: 'Auto Declined',
+    sortLabel: (direction: SortDirection) =>
+      direction === 'DESC' ? '(Descending)' : '(Ascending)',
+  },
+] as const
+
+export default function TradesDrawer() {
+  const [tradeStatusFilter, setTradeStatusFilter] = useState<TradeStatus>(
+    TRADE_STATUS_OPTIONS[1].value
+  )
+  const [partnerUsername, setPartnerUsername] = useState<string>('')
+  const [debouncedUsername] = useDebounce(partnerUsername, 500)
+  const { session, loggedIn } = useSession()
+
+  const { payload: loggedInTrades, isLoading } = query<ListResponse<Trade>>({
+    queryKey: [
+      'trades',
+      session?.token,
+      JSON.stringify(tradeStatusFilter),
+      debouncedUsername,
+    ],
+    queryFn: () =>
+      axios({
+        method: GET,
+        url: `/api/v3/trades`,
+        headers: { Authorization: `Bearer ${session?.token}` },
+        params: {
+          username: debouncedUsername?.length >= 3 ? debouncedUsername : '',
+          status: tradeStatusFilter,
+        },
+      }),
+    enabled: loggedIn,
+  })
+
+  return (
+    <>
+      <div className="border-b-8 border-b-blue700 bg-secondary p-4">
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          align={{ base: 'stretch', md: 'center' }}
+          justify="space-between"
+        >
+          <Heading as="h1" size="md" color="secondaryText" flexShrink={0}>
+            View Your Trades
+          </Heading>
+
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4} flex="1">
+            <FormControl>
+              <FormLabel>Status</FormLabel>
+              <Select
+                value={tradeStatusFilter}
+                onChange={(e) =>
+                  setTradeStatusFilter(e.target.value as TradeStatus)
+                }
+                className="bg-secondary text-primary"
+              >
+                {TRADE_STATUS_OPTIONS.map((option) => (
+                  <option
+                    className="!bg-secondary !text-primary"
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Partner</FormLabel>
+              <Input
+                placeholder="Partner username"
+                type="text"
+                value={partnerUsername}
+                onChange={(e) => setPartnerUsername(e.target.value)}
+                className="bg-secondary text-primary"
+              />
+            </FormControl>
+          </Stack>
+        </Stack>
+      </div>
+
+      <ViewTradeTable trades={loggedInTrades?.rows} isLoading={isLoading} />
+    </>
+  )
+}
