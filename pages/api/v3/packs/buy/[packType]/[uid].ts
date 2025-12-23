7443da7dddc5f86ff2ad900361c446e2b5a64641
luketd <lukedamato99@gmail.com>
2025-12-22 21:55:49 -0500

    Luke dumb

--- pages/api/v3/packs/buy/[packType]/[uid].ts
+++ pages/api/v3/packs/buy/[packType]/[uid].ts
@@ -90,14 +90,15 @@ const handler = async (
             WHERE uid=${uid};`
       )
 
-      const hasInsufficientFunds: boolean = assertBoom(
-        bankData[0]?.bankBalance > 0,
+      const hasInsufficientFunds = assertBoom(
+        packService.packs[packType].price === 0 ||
+          bankData[0]?.bankBalance >= packService.packs[packType].price,
         res,
         'Insufficient Funds',
         StatusCodes.BAD_REQUEST
       )
-      if (hasInsufficientFunds && packService.packs[packType].price !== 0)
-        return
+
+      if (hasInsufficientFunds) return
 
       if (packService.packs[packType].price !== 0) {
         await portalQuery(
